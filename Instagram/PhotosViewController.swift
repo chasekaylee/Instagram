c7cad993b3ad34bb0d78b3e7841967096f60a8ca
chasekaylee <chaseklee990@gmail.com>
2016-02-03 22:27:37 -0800

    added headers to include user prof pic and user name above uploaded image

--- Instagram/PhotosViewController.swift
+++ Instagram/PhotosViewController.swift
@@ -14,15 +14,57 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     @IBOutlet weak var userStreamTableView: UITableView!
     
     var photos: [NSDictionary]?
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         userStreamTableView.dataSource = self
         userStreamTableView.delegate = self
+        userStreamTableView.rowHeight = 320
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "getNewsFeed", forControlEvents: UIControlEvents.ValueChanged)
+        self.userStreamTableView.insertSubview(refreshControl, atIndex: 0)
+        
+        getNewsFeed()
 
         // Do any additional setup after loading the view.
         
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if let photos = photos {
+            return photos.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("UserCell", forIndexPath: indexPath) as! UserCell
+        
+        let photo = photos![indexPath.section]
+
+        let images = (photo["images"] as! NSDictionary)["standard_resolution"] as! NSDictionary
+        let photoUrl = NSURL(string: images["url"] as! String)
+        cell.photoView.setImageWithURL(photoUrl!)
+        
+        return cell
+    }
+    
+    
+    func getNewsFeed() {
         let clientId = "e05c462ebd86446ea48a5af73769b602"
         let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
         
@@ -46,50 +88,51 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                             
                             self.photos = responseDictionary["data"] as? [NSDictionary]
                             self.userStreamTableView.reloadData()
+                            self.refreshControl?.endRefreshing()
+
                     }
                 } else {
                     NSLog("Bad Request")
+                    print("Bad Request")
+                    self.refreshControl?.endRefreshing()
                 }
         });
         task.resume()
-    }
 
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let photos = photos {
-            return photos.count
-        } else {
-            return 0
-        }
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 50))
+        headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
+        
+        let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+        profileView.clipsToBounds = true
+        profileView.layer.cornerRadius = 15;
+        profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).CGColor
+        profileView.layer.borderWidth = 1;
+        
+        let photo = photos![section]
+        let user = photo["user"] as! NSDictionary
+        let photoUrl = NSURL(string: user["profile_picture"] as! String)
+        
+        profileView.setImageWithURL(photoUrl!)
+        
+        let usernameView = UILabel(frame: CGRect(x: 50, y: 10, width: 200, height: 30))
+        usernameView.clipsToBounds = true
+        usernameView.textColor = UIColor(red:0.022, green:0.21, blue:0.396, alpha:1)
+        
+        let username = user["username"] as! String
+        usernameView.text = username
+        
+        headerView.addSubview(profileView)
+        headerView.addSubview(usernameView)
+        
+        
+        return headerView
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier("UserCell", forIndexPath: indexPath) as! UserCell
-        
-        let photo = photos![indexPath.row]
-
-        
-        let images = (photo["images"] as! NSDictionary)["standard_resolution"] as! NSDictionary
-        let photoUrl = NSURL(string: images["url"] as! String)
-        cell.photoView.setImageWithURL(photoUrl!)
-        
-        return cell
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
